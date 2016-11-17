7a891b2296f0abc34403db4b670d6965563fce18
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-11-17 12:37:38 +0100

    guardado por cambio de pc

--- src/NumerosPrimos/NumerosPrimos.java
+++ src/NumerosPrimos/NumerosPrimos.java
@@ -16,7 +16,7 @@ public class NumerosPrimos {
     //LIstar los numeros según el numero de digitos indicado
     //Considero solo hasta numeros menores a 100000 (5 digitos), 
     //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
-    public static boolean p = false;
+    public static boolean esPrimo = false;
     static Scanner sc1 = new Scanner(System.in);
 
     public static void main(String arg[]) {
@@ -27,8 +27,8 @@ public class NumerosPrimos {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
 
-        for (int i = 1; i <= 99999; i++) {
-            int divisionEntera = i;
+        for (int contadorNumeros = 1; contadorNumeros <= 99999; contadorNumeros++) {
+            int divisionEntera = contadorNumeros;
 
             int contador = 0;
 
@@ -39,20 +39,20 @@ public class NumerosPrimos {
             ndigitos = contador;
 
             if (ndigitos == numDigitos) {
-                if (i < 4) {
-                    p = true;
-                } else if (i % 2 == 0) {
-                    p = false;
+                if (contadorNumeros < 4) {
+                    esPrimo = true;
+                } else if (contadorNumeros % 2 == 0) {
+                    esPrimo = false;
                 } else {
                     int contador1 = 0;
                     int i1 = 1;
-                    int limite = (i - 1) / 2;
+                    int limite = (contadorNumeros - 1) / 2;
                     if (limite % 2 == 0) {
                         limite--;
                     }
 
                     while (i1 <= limite) {
-                        if (i % i1 == 0) {
+                        if (contadorNumeros % i1 == 0) {
                             contador1++;
                         }
                         i1 += 2;
@@ -62,12 +62,12 @@ public class NumerosPrimos {
                     }
 
                     if (contador1 == 1) {
-                        p = true;
+                        esPrimo = true;
                     }
                 }
 
-                if (p == true) {
-                    System.out.println(i);
+                if (esPrimo == true) {
+                    System.out.println(contadorNumeros);
                 }
             }
         }
@@ -75,16 +75,27 @@ public class NumerosPrimos {
 
     public static int pedirNumDigitos() {
         int digit = 0;
+        System.out.println("Introduce el numero de digitos");
         while (digit <= 0 || digit > 5) {
-            System.out.println("Introduce el numero de digitos");
-            //try {
+            
+            try {
+                
                 digit = sc1.nextInt();
-            //} catch (Exception e) {
-            //    System.out.println("Mal");
-            //    sc1.reset();
-            //    digit =0;
-            //}
+            } catch (Exception e) {
+                
+                
+            System.out.println("Introduce un numero");                    
+            while (sc1.hasNextInt()==false){
+            
+            sc1.next();
+            }
+            }
+            
+            
+            
+        
         }
+        System.out.println("???");
         return digit;
     }
 
